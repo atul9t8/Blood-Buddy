796039cce75357934172f09472c34768cba18051
Atul Debnath <atul.debnath.dev@gmail.com>
2022-10-17 03:04:54 +0600

    registration and login done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors")
 const mongoose = require("mongoose")
 const app = express();
 app.use(express.json())
 const userRoute = require("./routers/userRouter")
 
+app.use(cors())
 require('dotenv').config()
 mongoose.connect(process.env.DB)
 
